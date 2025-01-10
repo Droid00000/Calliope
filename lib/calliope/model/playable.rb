6b6a25f9cf78b362ed6110ea1b7c21cdf1fcbbeb
Droid <johnship2876@gmail.com>
2025-01-10 03:26:56 -0500

    print it

--- lib/calliope/model/playable.rb
+++ lib/calliope/model/playable.rb
@@ -48,7 +48,7 @@ module Calliope
       end
 
       if @tracks && (type == :playlist && selected_track.nil?)
-        print(payload['pluginInfo'])
+        print payload['pluginInfo']
         @cover = payload.dig("pluginInfo", "artworkUrl") || @cover
         @artist = payload.dig("pluginInfo", "author") || @artist
         @source = payload.dig("pluginInfo", "url") || @source
